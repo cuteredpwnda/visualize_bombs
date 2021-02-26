ddc34db37e9002469df8df2ead3ff8c504e4f489
Jonas Neubürger <jonas793@gmail.com>
2021-02-26 19:52:01 +0100

    declutter

--- plotasmap.py
+++ plotasmap.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from pyproj import Proj, transform
 import utm
 import re
 
@@ -9,50 +8,55 @@ import re
 df = pd.read_csv('data/uebersichtslisteBombenfunde2017.csv', sep=';')
 
 # rename Koordinate1 and Koordinate2 to match what they are:
-df.rename(columns = {'Koordinate1': 'Latitude', 'Koordinate2': 'Longitude'}, inplace=True)
+df.rename(columns = {'Koordinate1': 'easting', 'Koordinate2': 'northing'}, inplace=True)
 
 #rewrite with . instead of ,
-for col in ['Latitude', 'Longitude']:
+for col in ['easting', 'northing']:
     df[col] = pd.to_numeric(df[col].apply(lambda x: re.sub(',' , '.', str(x))))
     
 #clean zeroes in diffrent df
-df_clean = df[df.Latitude != 0.00]
+df_clean = df[df.easting != 0.00]
 
 #fix wrong coordinates (too long, wrong decimal point probably)
-df_clean['Latitude'] = df_clean['Latitude'].apply(lambda x: round(x/10,2) if (x >= 1000000) else x)
-df_clean['Longitude'] = df_clean['Longitude'].apply(lambda x: round(x/10,2) if (x >= 1000000) else x)
-# twice, because at least one coordinate seems to be wrong by 2 decimal points
-df_clean['Longitude'] = df_clean['Longitude'].apply(lambda x: round(x/10,2) if (x >= 1000000) else x)
+df_clean['easting'] = df_clean['easting'].apply(lambda x: round(x/10,2) if (x >= 1000000) else x)
+df_clean['northing'] = df_clean['northing'].apply(lambda x: round(x/10,2) if (x > 10000000) else x)
+
+
+# create a new column:
+
+df_clean['(easting, northing)'] = list(zip(df_clean['easting'], df_clean['northing']))
+
+#create lists for easy access
+easting = df_clean['easting'].values.tolist()
+northing = df_clean['northing'].values.tolist()
 
 debug = False
 # debug prints, max and mins
 if (debug == True):
-    print('min, max Latitude')
-    print(df_clean['Latitude'].astype(int).min())
-    print(df_clean['Latitude'].min())
-    print(df_clean['Latitude'].astype(int).max())
-    print(df_clean['Latitude'].max())
-    print('min, max Longitude')
-    print(df_clean['Longitude'].astype(int).min())
-    print(df_clean['Longitude'].min())
-    print(df_clean['Longitude'].astype(int).max())
-    print(df_clean['Longitude'].max())
-
-# create a new column:
-df_clean['Latitude, Longitude (as UTM)'] = list(zip(df_clean['Latitude'], df_clean['Longitude']))
-column_latlon = df_clean['Latitude, Longitude (as UTM)']
-print('Latitude, Longitude (as UTM): ')
-print(column_latlon)
+    print('min, max easting')
+    print(df_clean['easting'].astype(int).min())
+    print(df_clean['easting'].min())
+    print(df_clean['easting'].astype(int).max())
+    print(df_clean['easting'].max())
+    print('min, max northing')
+    print(df_clean['northing'].astype(int).min())
+    print(df_clean['northing'].min())
+    print(df_clean['northing'].astype(int).max())
+    print(df_clean['northing'].max())
 
 #convert to lat/lon and int
-# 32, U is west germany
+# 32, U is roughly west germany
+latlon_list = []
+for i in range(len(easting)):
+    latlon_list.append(utm.to_latlon(int(round(easting[i])), int(round(northing[i])), 32, 'U'))
+    
+df_clean['(lat,lon) WGS84'] = latlon_list
 
-print(df_clean)
-
+df_clean['lat WGS84'], df_clean['lon WGS84'] = zip(*df_clean['(lat,lon) WGS84'])
 
 # find the map boundaries
-#BBox = (df_clean.Latitude.min(), df_clean.Latitude.max(),
-#            df_clean.Longitude.min(), df_clean.Longitude.max())
+BBox = (df_clean['lon WGS84'].min(), df_clean['lon WGS84'].max(),
+            df_clean['lat WGS84'].min(), df_clean['lat WGS84'].max())
+print(BBox)
 
 # load map
-#bomb_map = plt.imread('data/map.png')
